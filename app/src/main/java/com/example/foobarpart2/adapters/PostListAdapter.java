12fac1ad2adbd515bdde840f66c4868ceac1eac8
itayh <itay.hadad@live.biu.ad.il>
2024-02-15 18:57:57 +0200

    comments are workingngnngng

--- app/src/main/java/com/example/foobarpart2/adapters/PostListAdapter.java
+++ app/src/main/java/com/example/foobarpart2/adapters/PostListAdapter.java
@@ -81,7 +81,7 @@ public class PostListAdapter extends RecyclerView.Adapter<PostListAdapter.PostVi
             });
         }
 
-
+    //hello
     }
     private final LayoutInflater mInflater;
     private List<Post> posts;
